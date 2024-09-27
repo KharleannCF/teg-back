5b504407cbd16678748d1041395e760b533f1898
kharleanncf <kharleann@gmail.com>
2024-09-26 20:33:28 -0400

    Refactor getProyectos to include isCandidate and isPart properties in the returned projects

--- resources/proyecto/controller.js
+++ resources/proyecto/controller.js
@@ -28,10 +28,10 @@ export const getProyectos = async (req, res) => {
 
 		proyectos = proyectos.map((elem) => {
 			elem.isOwner = elem.user._id.toString() === req.user;
-			elem.isCandidate = elem.candidatos.find(
+			elem.isCandidate = !!elem.candidatos.find(
 				(subElem) => subElem._id.toString() === req.user
 			);
-			elem.isPart = elem.participantes.find(
+			elem.isPart = !!elem.participantes.find(
 				(subElem) => subElem._id.toString() === req.user
 			);
 			return elem;
